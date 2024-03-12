ac22500ac301999369d1319b86d3dd9ee2f8f863
DIEG0MARIN <diegof.maring@konradlorenz.edu.co>
2024-03-11 21:16:56 -0500

    Desarrollo backend primera parte de registro medicos

--- app.js
+++ app.js
@@ -1,128 +1,62 @@
-/*// obteniendo la informacion por medio del DOM y los IDs
-const numeroDocumento = document.getElementById("Número de Documento")
-const nombres = document.getElementById("Nombres y apellidos")
-const rol = document.getElementById("Perfil")
-const telefono = document.getElementById("Teléfono")
-const correo = document.getElementById("Correo Institucional")
-const dependencia = document.getElementById("Facultad a la que Pertenece")
-const portatil = document.getElementById("Número de Portátil que se le Presta")
-const fecha = document.getElementById("Fecha de Préstamo")
-const nota = document.getElementById("Observaciones del Equipo Portátil")
+// Obteniendo la información por medio del DOM y los IDS
 
-//llamado de los formularios
-const formularioPrestamo = document.getElementById("registro-prestamo")
-const formularioDevolucion = document.getElementById("registro-devolucion")
+// Campos compartidos entre pacientes y médicos
+const nombres = document.getElementById("nombres")
+const apellidos = document.getElementById("apellidos")
+const cedula = document.getElementById("cedula")
+const telefono = document.getElementById("telefono")
+const especialidad = document.getElementById("especialidad")
 
 
+// Campo propio de los médicos
+
+const consultorio = document.getElementById("consultorio")
+const correo = document.getElementById("correo")
+
+// Campos propios de los pacientes
+
+const edad = document.getElementById("edad")
+
+// Llamado de los formularios
+
+const formularioMedicos = document.getElementById("registro-medicos-form")
+
+const formularioPacientes = document.getElementById("registro-pacientes-form")
 
 class Usuario{
-    constructor( numeroDocumento,nombres, rol, telefono, correo, dependencia, portatil, fecha, nota){
-        this.numeroDocumento = numeroDocumento
+
+    constructor(nombres, apellidos, cedula, telefono, especialidad){
         this.nombres = nombres
-        this.rol = rol
+        this.apellidos = apellidos
+        this.cedula = cedula
         this.telefono = telefono
-        this.correo = correo 
-        this.dependencia = dependencia
-        this.portatil = portatil
-        this.fecha = fecha
-        this.nota = nota
+        this.especialidad = especialidad
+
     }
+
 }
 
-//el evento  para formularioPrestamo va a ser de tipo enviar o guardar es decir submit
-formularioPrestamo.addEventListener("submit" , function(event){
+///El evento para formularioMedicos va a ser de tipo enviar o guardar es decir de tipo submit
+formularioMedicos.addEventListener("submit", function(){
 
-    //previene que la página se recargue sin antes hacer la logica del addEventlistener
+    //Previene que la pagina se recargue sin antes hacer la logica del addEventListener
     event.preventDefault()
 
-    let valornumeroDocumento = numeroDocumento.value
+
     let valorNombres = nombres.value
-    let valorrol = rol.value
-    let valortelefono = telefono.value
-    let valorcorreo = correo.value
-    let valordependencia = dependencia.value
-    let valorportatil = portatil.value
-    let valorfecha = fecha.value
-    let valornota = nota.value
+    let valorApellidos = apellidos.value
+    let valorCedula = cedula.value
+    let valorConsultorio = consultorio.value
+    let valorTelefono = telefono.value
+    let valorCorreo = correo.value
+    let valorEspecialidad = especialidad.value
 
-    // se crea un objeto llamado prestamo
+    const medico  = new Usuario(valorNombres, valorApellidos ,valorCedula, valorConsultorio,valorTelefono , valorCorreo, valorEspecialidad)
+    medico.consultorio = valorConsultorio
+    medico.correo = valorCorreo
 
-    const prestamo = new Usuario(valornumeroDocumento,valorNombres,valorrol,valortelefono,valorcorreo,valordependencia,valorportatil,valorfecha,valornota)
+    console.log(medico)
+    alert("Medico registrado")
+    
 
-    alert("Préstamo registrado con exito")
-    console.log(prestamo)
-})*/
-
-// obteniendo la informacion por medio del DOM y los IDs
-const numeroDocumento = document.getElementById("numero_id");
-const nombres = document.getElementById("nombre");
-const rol = document.getElementById("perfil");
-const telefono = document.getElementById("telefono");
-const correo = document.getElementById("email");
-const dependencia = document.getElementById("facultad");
-const portatil = document.getElementById("numero_portatil");
-const fecha = document.getElementById("fecha_prestamo");
-const nota = document.getElementById("observaciones");
-
-// llamado de los formularios
-const formularioPrestamo = document.getElementById("registro-prestamo");
-const formularioDevolucion = document.getElementById("registro-devolucion");
-
-class Usuario{
-    constructor( numeroDocumento,nombres, rol, telefono, correo, dependencia, portatil, fecha, nota){
-        this.numeroDocumento = numeroDocumento
-        this.nombres = nombres
-        this.rol = rol
-        this.telefono = telefono
-        this.correo = correo 
-        this.dependencia = dependencia
-        this.portatil = portatil
-        this.fecha = fecha
-        this.nota = nota
-    }
-}
-// ...
-
-// el evento  para formularioPrestamo va a ser de tipo enviar o guardar, es decir, submit
-formularioPrestamo.addEventListener("submit", function(event) {
-    // previene que la página se recargue sin antes hacer la lógica del addEventlistener
-    event.preventDefault();
-
-    let valornumeroDocumento = numeroDocumento.value;
-    let valorNombres = nombres.value;
-    let valorrol = rol.value;
-    let valortelefono = telefono.value;
-    let valorcorreo = correo.value;
-    let valordependencia = dependencia.value;
-    let valorportatil = portatil.value;
-    let valorfecha = fecha.value;
-    let valornota = nota.value;
-
-    // se crea un objeto llamado prestamo
-    const prestamo = new Usuario(
-        valornumeroDocumento,
-        valorNombres,
-        valorrol,
-        valortelefono,
-        valorcorreo,
-        valordependencia,
-        valorportatil,
-        valorfecha,
-        valornota
-    );
-
-    let prestamos = []
-
-    let localPrestamos = localStorage.getItem("prestamos")
-    // si localprestamos no esta vacio lo convierte a objeto para hacer un push 
-
-    if (localPrestamos){
-        prestamos = JSON.parse(localPrestamos)
-    }
-    prestamos.push(prestamo)
-    localStorage.setItem("prestamos", JSON.stringify(prestamo))
-    alert("Prestamo regitrado")
-    /*alert("Préstamo registrado con éxito");
-    console.log(prestamo);*/
-});
-
+})
